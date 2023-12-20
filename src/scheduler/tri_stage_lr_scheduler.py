d84e0fa0f7b78a2c88b8cbc62e9a5abb09e157ac
molivier <malo.olivier@insa-lyon.fr>
2023-12-20 11:13:53 +0100

    removes parent class LearningRateScheduler and modifies TriStageLRScheduler class

--- src/scheduler/tri_stage_lr_scheduler.py
+++ src/scheduler/tri_stage_lr_scheduler.py
@@ -20,43 +20,12 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-import math
 import torch
 from typing import Optional
 from torch.optim import Optimizer
-from src.scheduler.lr_scheduler import LearningRateScheduler
-
-
 from torch.optim.lr_scheduler import _LRScheduler
 
-
-class LearningRateScheduler(_LRScheduler):
-    r"""
-    Provides inteface of learning rate scheduler.
-
-    Note:
-        Do not use this class directly, use one of the sub classes.
-    """
-
-    def __init__(self, optimizer, lr):
-        self.optimizer = optimizer
-        self.lr = lr
-
-    def step(self, *args, **kwargs):
-        raise NotImplementedError
-
-    @staticmethod
-    def set_lr(optimizer, lr):
-        for g in optimizer.param_groups:
-            g["lr"] = lr
-
-    def get_lr(self):
-        for g in self.optimizer.param_groups:
-            return g["lr"]
-
-
-
-class TriStageLRScheduler(LearningRateScheduler):
+class TriStageLRScheduler(_LRScheduler):
     r"""
     Tri-Stage Learning Rate Scheduler. Implement the learning rate scheduler in "SpecAugment"
 
@@ -91,7 +60,9 @@ class TriStageLRScheduler(LearningRateScheduler):
         assert isinstance(hold_steps, int), "hold_steps should be integer type"
         assert isinstance(decay_steps, int), "decay_steps should be integer type"
 
-        super(TriStageLRScheduler, self).__init__(optimizer, init_lr)
+        super(_LRScheduler, self).__init__()
+        self.optimizer = optimizer
+        self.lr = init_lr
         self.init_lr = init_lr
         self.init_lr *= init_lr_scale
         self.final_lr = final_lr
@@ -152,3 +123,12 @@ class TriStageLRScheduler(LearningRateScheduler):
         self.update_steps += 1
 
         return self.lr
+
+    @staticmethod
+    def set_lr(optimizer, lr):
+        for g in optimizer.param_groups:
+            g["lr"] = lr
+
+    def get_lr(self):
+        for g in self.optimizer.param_groups:
+            return g["lr"]
