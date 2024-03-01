e1f070704e99ab62c0014b208b9acecfa45b54ed
Julien Hauret <j.hauret.33@gmail.com>
2024-03-01 16:40:52 +0100

    raise LibsndfileError & new format

--- src/data/bwe_lightning_datamodule.py
+++ src/data/bwe_lightning_datamodule.py
@@ -5,11 +5,13 @@ from torch.utils.data import DataLoader
 
 
 class BWELightningDataModule(pl.LightningDataModule):
+
+    DATASET_NAME = "Cnam-LMSSC/vibravox"
+
     def __init__(
         self,
-        dataset_name: str = "Cnam-LMSSC/vibravox",
-        config_name="BWE_In-ear_Comply_Foam_microphone",
-        streaming=False,
+        config_name="bwe_in-ear_rigid_earpiece_microphone",
+        streaming=True,
         sample_rate=16000,
         batch_size=32,
         num_workers=4,
@@ -18,7 +20,6 @@ class BWELightningDataModule(pl.LightningDataModule):
         LightningDataModule for Bandwidth Extension (BWE)
 
         Args:
-            dataset_name (str, optional): Name of the dataset. Defaults to "Cnam-LMSSC/vibravox".
             config_name (str, optional): Name of the configuration. Defaults to "BWE_In-ear_Comply_Foam_microphone".
             streaming (bool, optional): If True, the audio files are dynamically downloaded. Defaults to False.
             sample_rate (int, optional): Sample rate of the audio files. Defaults to 16000.
@@ -27,7 +28,6 @@ class BWELightningDataModule(pl.LightningDataModule):
         """
         super().__init__()
 
-        self.dataset_name = dataset_name
         self.config_name = config_name
         self.sample_rate = sample_rate
         self.streaming = streaming
@@ -36,19 +36,22 @@ class BWELightningDataModule(pl.LightningDataModule):
 
     def setup(self, stage=None):
         datasets = load_dataset(
-            self.dataset_name, self.config_name, streaming=self.streaming
+            self.DATASET_NAME, self.config_name, streaming=self.streaming
         )
         datasets = datasets.remove_columns(
             [
                 "audio_length",
-                "gender",
+                "transcription",
+                "text",
+                "is_gold_transcript",
                 "num_channels",
-                "type",
                 "sensor_id",
+                "speaker_id",
+                "gender",
+                "is_speech",
+                "is_noisy",
                 "split",
                 "sentence_id",
-                "transcription",
-                "speaker_id",
             ]
         )
         datasets = datasets.cast_column("audio", Audio(sampling_rate=self.sample_rate))
--- tests/data/bwe_lightning_datamodule_test.py
+++ tests/data/bwe_lightning_datamodule_test.py
@@ -1,15 +1,22 @@
+import os
 import torch
+import lightning
 import hydra
 import pytest
 from src.data.bwe_lightning_datamodule import BWELightningDataModule
 
 
+@pytest.fixture(params=os.listdir("./configs/lightning_datamodule"))
+def datamodule_config_name(request) -> str:
+    return request.param
+
 @pytest.fixture
-def dataset_name() -> str:
-    return "Cnam-LMSSC/vibravox"
+def hydra_lightning_datamodule_instance(datamodule_config_name) -> lightning.LightningModule:
+    with hydra.initialize(version_base="1.3", config_path="./configs/lightning_datamodule"):
+        cfg = hydra.compose(config_name=datamodule_config_name)
+        return hydra.utils.instantiate(cfg)
 
-
-@pytest.fixture(params=["BWE_In-ear_Comply_Foam_microphone"])
+@pytest.fixture(params=["bwe_in-ear_rigid_earpiece_microphone"])
 def config_name(request) -> str:
     return request.param
 
@@ -39,12 +46,12 @@ def num_workers(request) -> int:
 
 @pytest.fixture
 def bwe_lightning_datamodule_instance(
-    dataset_name, config_name, streaming, sample_rate, batch_size, num_workers
+    config_name, streaming, sample_rate, batch_size, num_workers
 ) -> BWELightningDataModule:
     """BWELightningDataModule instance."""
 
     datamodule = BWELightningDataModule(
-        dataset_name, config_name, streaming, sample_rate, batch_size, num_workers
+        config_name, streaming, sample_rate, batch_size, num_workers
     )
 
     return datamodule
@@ -52,6 +59,13 @@ def bwe_lightning_datamodule_instance(
 
 class TestBWELightningDataModule:
     def test_dataset_returns_torch_tensor(self, bwe_lightning_datamodule_instance):
+        bwe_lightning_datamodule_instance.setup()
+        train_dataset = bwe_lightning_datamodule_instance.train_dataset
+        sample = next(iter(train_dataset))
+
+        assert isinstance(sample["audio"], torch.Tensor)
+
+    def test_hydra_instantiation(self, hydra_lightning_datamodule_instance):
         bwe_lightning_datamodule_instance.setup()
         train_dataset = bwe_lightning_datamodule_instance.train_dataset
         sample = next(iter(train_dataset))
