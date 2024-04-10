43cb064732f095ef117406e7fa8127635a31b910
jhauret <j.hauret.33@gmail.com>
2024-04-10 18:10:17 +0200

    launching script

---
+++ tmp/run_all_stp.sh
@@ -0,0 +1,6 @@
+python train.py lightning_datamodule=stp lightning_module=wav2vec2_for_stp lightning_module.optimizer.lr=1e-5 lightning_datamodule.sensor=airborne.mouth_headworn.reference_microphone ++trainer.max_epochs=10;
+python train.py lightning_datamodule=stp lightning_module=wav2vec2_for_stp lightning_module.optimizer.lr=1e-5 lightning_datamodule.sensor=body_conducted.forehead.miniature_accelerometer ++trainer.max_epochs=10;
+python train.py lightning_datamodule=stp lightning_module=wav2vec2_for_stp lightning_module.optimizer.lr=1e-5 lightning_datamodule.sensor=body_conducted.in_ear.comply_foam_microphone ++trainer.max_epochs=10;
+python train.py lightning_datamodule=stp lightning_module=wav2vec2_for_stp lightning_module.optimizer.lr=1e-5 lightning_datamodule.sensor=body_conducted.in_ear.rigid_earpiece_microphone ++trainer.max_epochs=10;
+python train.py lightning_datamodule=stp lightning_module=wav2vec2_for_stp lightning_module.optimizer.lr=1e-5 lightning_datamodule.sensor=body_conducted.temple.contact_microphone ++trainer.max_epochs=10;
+python train.py lightning_datamodule=stp lightning_module=wav2vec2_for_stp lightning_module.optimizer.lr=1e-5 lightning_datamodule.sensor=body_conducted.throat.piezoelectric_sensor ++trainer.max_epochs=10;
