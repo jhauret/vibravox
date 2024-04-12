9b26300d42d57911bc5e64d90ac65e805993c231
jhauret <j.hauret.33@gmail.com>
2024-04-12 14:21:38 +0200

    fixing test

--- tests/lightning_datamodules/bwe_test.py
+++ tests/lightning_datamodules/bwe_test.py
@@ -15,17 +15,17 @@ class TestBWELightningDataModule:
         train_dataloder = bwe_lightning_datamodule_instance.train_dataloader()
         dataloader_sample = next(iter(train_dataloder))
 
-        assert isinstance(dataloader_sample, list), "Expected a list."
+        assert isinstance(dataloader_sample, dict), "Expected a dict."
         assert all(
             [
-                isinstance(dataloader_sample[0], torch.Tensor),
-                isinstance(dataloader_sample[1], torch.Tensor),
+                isinstance(dataloader_sample["audio_body_conducted"], torch.Tensor),
+                isinstance(dataloader_sample["audio_airborne"], torch.Tensor),
             ]
         ), "Expected all elements in the list to be torch.Tensor."
         assert (
-            dataloader_sample[0].shape == dataloader_sample[1].shape
+            dataloader_sample["audio_body_conducted"].shape == dataloader_sample["audio_airborne"].shape
         ), "Expected the same number of samples in both tensors."
-        assert dataloader_sample[0].dim() == 3, "Expected 3 dimensions in the tensor."
+        assert dataloader_sample["audio_body_conducted"].dim() == 3, "Expected 3 dimensions in the tensor."
 
     def test_no_offset_between_audio_samples(self, bwe_lightning_datamodule_instance):
         if bwe_lightning_datamodule_instance.subset == "speech_noisy":
@@ -34,8 +34,8 @@ class TestBWELightningDataModule:
         train_dataloder = bwe_lightning_datamodule_instance.train_dataloader()
         dataloader_sample = next(iter(train_dataloder))
 
-        corrupted_audio = dataloader_sample[0][0:1, 0:1, :]
-        reference_audio = dataloader_sample[1][0:1, 0:1, :]
+        corrupted_audio = dataloader_sample["audio_body_conducted"][0:1, 0:1, :]
+        reference_audio = dataloader_sample["audio_airborne"][0:1, 0:1, :]
 
         # Note: applying remove_hf on reference_audio and remove_bf on corrupted audio is not necessary
 
