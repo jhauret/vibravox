85f79f9f79edd1dbb5da0eb034a77b2a03efdce0
jhauret <j.hauret.33@gmail.com>
2024-04-15 12:03:46 +0200

    learning_strategy & dynamic_loss_balancing

--- vibravox/lightning_modules/eben.py
+++ vibravox/lightning_modules/eben.py
@@ -1,5 +1,5 @@
 from functools import partial
-from typing import Any, Dict
+from typing import Any, Dict, List
 
 import auraloss
 import torch
@@ -24,6 +24,8 @@ class EBENLightningModule(LightningModule):
         generator_optimizer: partial[torch.optim.Optimizer],
         discriminator_optimizer: partial[torch.optim.Optimizer],
         metrics: MetricCollection,
+        learning_strategy: str = "all",
+        dynamic_loss_balancing: str = None,
     ):
         """
         Definition of EBEN and its training pipeline with pytorch lightning paradigm
@@ -35,6 +37,15 @@ class EBENLightningModule(LightningModule):
             generator_optimizer (partial[torch.optim.Optimizer]): Optimizer for the generator
             discriminator_optimizer (partial[torch.optim.Optimizer]): Optimizer for the discriminator
             metrics (MetricCollection): Metrics to be computed.
+            learning_strategy (str): Strategy to train the model. Default is "all". Options are:
+             - "all": Train both generator and discriminator with all losses
+             - "rec_only": Train only the generator with reconstructive losses
+             - "adv_only": Train both generator and discriminator with adversarial and feature matching losses
+            dynamic_loss_balancing (str): Whether to automatically balance the losses w.r.t. their gradients. One of:
+             - None: Do not balance the losses
+             - "simple": Balance the losses by dividing them by the gradient norm
+             - "ema": Balance the losses by dividing them by the exponential moving average of the gradient norm
+             Default: None
         """
         super().__init__()
 
@@ -47,18 +58,28 @@ class EBENLightningModule(LightningModule):
         self.discriminator_optimizer: torch.optim.Optimizer = discriminator_optimizer(
             params=self.discriminator.parameters()
         )
-        self.feature_matching_loss_fn = FeatureLossForDiscriminatorMelganMultiScales()
-        self.adversarial_loss_fn = HingeLossForDiscriminatorMelganMultiScales()
-        self.reconstructive_loss_temp_fn = torch.nn.L1Loss()
-        self.reconstructive_loss_freq_fn = auraloss.freq.MultiResolutionSTFTLoss(
-            fft_sizes=[1024, 2048, 8192],
-            hop_sizes=[256, 512, 2048],
-            win_lengths=[1024, 2048, 8192],
-            scale="mel",
-            n_bins=128,
-            sample_rate=self.sample_rate,
-            perceptual_weighting=True,
-        )
+
+        assert learning_strategy in {'all', 'rec_only', 'adv_only'}, "learning_strategy must be in {'all', 'rec_only', 'adv_only'}"
+        self.learning_strategy = learning_strategy
+
+        if self.learning_strategy in {'all', 'rec_only'}:
+            self.reconstructive_loss_temp_fn = torch.nn.L1Loss()
+            self.reconstructive_loss_freq_fn = auraloss.freq.MultiResolutionSTFTLoss(
+                fft_sizes=[1024, 2048, 8192],
+                hop_sizes=[256, 512, 2048],
+                win_lengths=[1024, 2048, 8192],
+                scale="mel",
+                n_bins=128,
+                sample_rate=self.sample_rate,
+                perceptual_weighting=True,
+            )
+        if self.learning_strategy in {'all', 'adv_only'}:
+            self.feature_matching_loss_fn = FeatureLossForDiscriminatorMelganMultiScales()
+            self.adversarial_loss_fn = HingeLossForDiscriminatorMelganMultiScales()
+
+        assert dynamic_loss_balancing in {None, "simple", "ema"}, "dynamic_loss_balancing must be in {None, 'simple', 'ema'}"
+        self.dynamic_loss_balancing = dynamic_loss_balancing
+        self.lambdas_past = None  # For dynamic loss balancing
 
         self.metrics: MetricCollection = metrics
 
@@ -90,44 +111,43 @@ class EBENLightningModule(LightningModule):
 
         # Initialize step_output
         step_output = {
-            # "loss": None, we don't need this because we are using manual optimization
-            "audio": {
                 f"corrupted": corrupted_speech,
                 f"enhanced": enhanced_speech,
                 f"reference": reference_speech,
-            },
-            "scalars_to_log": dict(),
-        }
+            }
 
-        enhanced_embeddings = self.discriminator(bands=decomposed_enhanced_speech, audio=enhanced_speech)
-        reference_embeddings = self.discriminator(bands=decomposed_reference_speech, audio=reference_speech)
+        atomic_losses = dict()
 
-        # Compute adversarial_loss
-        #adv_loss_gen = self.adversarial_loss_fn(embeddings=enhanced_embeddings, target=1)
+        # Compute losses that are using the discriminator
+        if self.learning_strategy in {"all", "adv_only"}:
+            enhanced_embeddings = self.discriminator(
+                bands=decomposed_enhanced_speech, audio=enhanced_speech
+            )
+            reference_embeddings = self.discriminator(
+                bands=decomposed_reference_speech, audio=reference_speech
+            )
 
-        # Compute feature_matching_loss
-        #feature_matching_loss = self.feature_matching_loss_fn(enhanced_embeddings, reference_embeddings)
+            atomic_losses["adv_loss_gen"] = self.adversarial_loss_fn(embeddings=enhanced_embeddings, target=1)
+            atomic_losses["feature_matching_loss"] = self.feature_matching_loss_fn(enhanced_embeddings, reference_embeddings)
 
-        # Compute reconstructive_loss_temp
-        reconstructive_loss_temp = self.reconstructive_loss_temp_fn(enhanced_speech, reference_speech)
+            self.log("train/generator/adv_loss", atomic_losses["adv_loss_gen"])
+            self.log("train/generator/feature_matching_loss", atomic_losses["feature_matching_loss"])
 
-        # Compute reconstructive_loss_freq
-        reconstructive_loss_freq = self.reconstructive_loss_freq_fn(enhanced_speech, reference_speech)
+        # Compute reconstructive losses
+        if self.learning_strategy in {"all", "rec_only"}:
 
-        # Compute loss to backprop on
-        backprop_loss_gen = (
-            #adv_loss_gen
-            #+ feature_matching_loss
-            reconstructive_loss_temp
-            + reconstructive_loss_freq
-        )
+            atomic_losses["reconstructive_loss_temp"] = self.reconstructive_loss_temp_fn(enhanced_speech, reference_speech)
+            atomic_losses["reconstructive_loss_freq"] = self.reconstructive_loss_freq_fn(enhanced_speech, reference_speech)
 
-        # Log scalars
-        #self.log("train/generator/adv_loss", adv_loss_gen)
-        #self.log("train/generator/feature_matching_loss", feature_matching_loss)
-        self.log("train/generator/reconstructive_loss_temp", reconstructive_loss_temp)
-        self.log("train/generator/reconstructive_loss_freq", reconstructive_loss_freq)
-        self.log("train/generator/total_loss", backprop_loss_gen)
+            self.log("train/generator/reconstructive_loss_temp", atomic_losses["reconstructive_loss_temp"])
+            self.log("train/generator/reconstructive_loss_freq", atomic_losses["reconstructive_loss_freq"])
+
+        # Dynamically balance losses
+        if self.dynamic_loss_balancing is not None:
+            atomic_losses = self.dynamically_balance_losses(atomic_losses)
+
+        backprop_loss_gen = sum(atomic_losses.values())
+        self.log("train/generator/backprop_loss", backprop_loss_gen)
 
         self.manual_backward(backprop_loss_gen)
         generator_optimizer.step()
@@ -135,30 +155,31 @@ class EBENLightningModule(LightningModule):
         self.untoggle_optimizer(generator_optimizer)
 
         ######################## Train Discriminator ########################
-        # self.toggle_optimizer(discriminator_optimizer)
-        #
-        # # Compute forwards again is necessary because we haven't retain_graph
-        # enhanced_embeddings = self.discriminator(
-        #     bands=decomposed_enhanced_speech.detach(), audio=enhanced_speech.detach()
-        # )
-        # reference_embeddings = self.discriminator(
-        #     bands=decomposed_reference_speech, audio=reference_speech
-        # )
-        #
-        # # Compute adversarial_loss
-        # real_loss = self.adversarial_loss_fn(embeddings=reference_embeddings, target=1)
-        # fake_loss = self.adversarial_loss_fn(embeddings=enhanced_embeddings, target=-1)
-        #
-        # # Compute and log total loss
-        # backprop_loss_dis = real_loss + fake_loss
-        #
-        # # Log scalars
-        # self.log("train/discriminator/total_loss", real_loss)
-        #
-        # self.manual_backward(backprop_loss_dis)
-        # discriminator_optimizer.step()
-        # discriminator_optimizer.zero_grad()
-        # self.untoggle_optimizer(discriminator_optimizer)
+        if self.learning_strategy in {"all", "adv_only"}:
+            self.toggle_optimizer(discriminator_optimizer)
+
+            # Compute forwards again is necessary because we haven't retain_graph
+            enhanced_embeddings = self.discriminator(
+                bands=decomposed_enhanced_speech.detach(), audio=enhanced_speech.detach()
+            )
+            reference_embeddings = self.discriminator(
+                bands=decomposed_reference_speech, audio=reference_speech
+            )
+
+            real_loss = self.adversarial_loss_fn(embeddings=reference_embeddings, target=1)
+            fake_loss = self.adversarial_loss_fn(embeddings=enhanced_embeddings, target=-1)
+
+            backprop_loss_dis = real_loss + fake_loss
+
+            # Log scalars
+            self.log("train/discriminator/real_loss", real_loss)
+            self.log("train/discriminator/fake_loss", fake_loss)
+            self.log("train/discriminator/backprop_loss", backprop_loss_dis)
+
+            self.manual_backward(backprop_loss_dis)
+            discriminator_optimizer.step()
+            discriminator_optimizer.zero_grad()
+            self.untoggle_optimizer(discriminator_optimizer)
 
         return step_output
 
@@ -215,24 +236,23 @@ class EBENLightningModule(LightningModule):
         enhanced_speech, _ = self.generator(corrupted_speech)
 
         step_output = {
-            "audio": {
                 f"corrupted": corrupted_speech,
                 f"enhanced": enhanced_speech,
                 f"reference": reference_speech,
-            },
-            "scalars_to_log": dict(),
-        }
+            }
 
         return step_output
 
     def common_eval_logging(self, stage, outputs, batch_idx):
 
         assert stage in ["validation", "test"], "stage must be in ['validation', 'test']"
-        assert "audio" in outputs, "audio key must be in outputs"
+        assert "corrupted" in outputs, "corrupted must be in outputs"
+        assert "enhanced" in outputs, "enhanced must be in outputs"
+        assert "reference" in outputs, "reference must be in outputs"
 
         # Log metrics
         metrics_to_log = self.metrics(
-            outputs["audio"]["enhanced"], outputs["audio"]["reference"]
+            outputs["enhanced"], outputs["reference"]
         )
         metrics_to_log = {f"{stage}/{k}": v for k, v in metrics_to_log.items()}
         self.log_dict(
@@ -244,7 +264,7 @@ class EBENLightningModule(LightningModule):
         if self.trainer.global_step % 10 == 0:
             # Log audio
             self.log_audio(
-                prefix=f"{stage}/", speech_dict=outputs["audio"], batch_idx=batch_idx
+                prefix=f"{stage}/", speech_dict=outputs, batch_idx=batch_idx
             )
 
     @staticmethod
@@ -273,3 +293,41 @@ class EBENLightningModule(LightningModule):
                     global_step=self.trainer.global_step + batch_idx,
                     sample_rate=self.sample_rate,
                 )
+
+    def compute_lambdas(
+        self,
+        atomic_losses: Dict[str, torch.Tensor],
+        loss_adjustment_layer: torch.Tensor,
+        beta: float = 0.999,
+    ):
+        """
+        Compute the adaptive lambdas to balance the losses
+
+        Args:
+            atomic_losses (Dict[torch.Tensor]): List of atomic losses
+            loss_adjustment_layer (torch.Tensor): Parameters of nn.Module where gradients are computed to adapt lambdas
+            beta (float): Beta parameter for the exponential moving average. Only used if ema=True. Default: 0.99
+
+        Returns:
+            List[torch.Tensor]: List of lambdas
+        """
+
+        lambdas = []
+
+        for atomic_loss in atomic_losses.values():
+            atomic_grad = torch.autograd.grad(
+                outputs=atomic_loss, inputs=loss_adjustment_layer, retain_graph=True
+            )[0]
+            lambda_adaptive = 1 / (torch.norm(atomic_grad) + 1e-4)
+            lambdas.append(torch.clamp(lambda_adaptive, 0.0, 1e4).detach())
+
+        if self.dynamic_loss_balancing == "ema":
+            if self.lambdas_past is None:
+                self.lambdas_past = lambdas
+            else:
+                lambdas = [
+                    beta * lambda_past + (1 - beta) * lambda_
+                    for lambda_past, lambda_ in zip(self.lambdas_past, lambdas)
+                ]
+
+        return lambdas
