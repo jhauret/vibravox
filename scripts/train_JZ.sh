0c51662e880b6a3c9b785d0e1a695cfdf0952c75
jhauret <j.hauret.33@gmail.com>
2024-04-20 14:58:12 +0200

    ALL losses on JZ

---
+++ scripts/train_JZ.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#SBATCH --job-name=vibravox_bwe
+#SBATCH --output=vibravox_bwe%j.out
+#SBATCH --error=vibravox_bwe%j.err
+#SBATCH --constraint=v100-16g
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=10
+#SBATCH --time=01:00:00
+#SBATCH --qos=qos_gpu-dev
+#SBATCH --hint=nomultithread
+#SBATCH --account=lbo@v100
+
+module purge                                          # nettoyer les modules herites par defaut
+conda deactivate                                      # desactiver les environnements herites par defaut
+
+module load pytorch-gpu/py3/2.2.0                     # charger les modules
+
+export HF_DATATSETS_CACHE=$WORK/huggingface_cache/datasets
+export HF_DATASETS_OFFLINE=1
+
+set -x                                                # activer l'echo des commandes
+srun python -u train.py lightning_datamodule=bwe lightning_module=eben ++trainer.check_val_every_n_epoch=15
