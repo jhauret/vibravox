5eb252a028b743284e5fec3b49fff7b5bff3035b
jhauret <j.hauret.33@gmail.com>
2024-05-04 09:47:48 +0200

    qos dev

--- scripts/run_all_bwe_JZ.sh
+++ scripts/run_all_bwe_JZ.sh
@@ -30,8 +30,8 @@ for SENSOR in "${SENSORS[@]}"; do
     #SBATCH --ntasks=1
     #SBATCH --gres=gpu:1
     #SBATCH --cpus-per-task=10
-    #SBATCH --time=99:00:00
-    #SBATCH --qos=qos_gpu-t4
+    #SBATCH --time=00:30:00
+    #SBATCH --qos=qos_gpu-dev
     #SBATCH --hint=nomultithread
     #SBATCH --account=lbo@v100
 
