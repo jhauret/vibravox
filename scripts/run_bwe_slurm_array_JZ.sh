62996a024567c20e6a3960d7cf0fddf10d7e004f
jhauret <j.hauret.33@gmail.com>
2024-05-04 11:18:35 +0200

    99h

--- scripts/run_bwe_slurm_array_JZ.sh
+++ scripts/run_bwe_slurm_array_JZ.sh
@@ -8,8 +8,8 @@
 #SBATCH --ntasks=1
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=10
-#SBATCH --time=00:01:00
-#SBATCH --qos=qos_gpu-dev
+#SBATCH --time=99:00:00
+#SBATCH --qos=qos_gpu-t4
 #SBATCH --hint=nomultithread
 #SBATCH --account=lbo@v100
 #SBATCH --array=1-5
