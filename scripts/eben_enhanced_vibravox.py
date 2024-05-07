e9fbc451c9da3816712f23c5541ed365dc81b8c1
jhauret <j.hauret.33@gmail.com>
2024-05-07 16:42:57 +0200

    script is working

--- scripts/eben_enhanced_vibravox.py
+++ scripts/eben_enhanced_vibravox.py
@@ -1,30 +1,39 @@
 # TODO: finish to implement the script
 
 import torch
+import torchaudio
 from datasets import load_dataset
 
 from vibravox.torch_modules.dnn.eben_generator import EBENGenerator
 
-ebens = {"forehead": EBENGenerator(4, 4, 4), #EBENGenerator.from_pretrained(f"Cnam-LMSSC/path_to_eben")
-         "rigid_inear": EBENGenerator(4, 4, 4),
-         "soft_inear": EBENGenerator(4, 4, 4),
-         "throat": EBENGenerator(4, 4, 4),
-         "temple": EBENGenerator(4, 4, 4)}
+EBENs = {"body_conducted.forehead.miniature_accelerometer": EBENGenerator.from_pretrained(f"Cnam-LMSSC/EBEN_dummy"),
+         "body_conducted.in_ear.rigid_earpiece_microphone": EBENGenerator.from_pretrained(f"Cnam-LMSSC/EBEN_dummy"),
+         "body_conducted.in_ear.comply_foam_microphone": EBENGenerator.from_pretrained(f"Cnam-LMSSC/EBEN_dummy"),
+         "body_conducted.throat.piezoelectric_sensor": EBENGenerator.from_pretrained(f"Cnam-LMSSC/EBEN_dummy"),
+         "body_conducted.temple.contact_microphone": EBENGenerator.from_pretrained(f"Cnam-LMSSC/EBEN_dummy")}
+
+resampler = torchaudio.transforms.Resample(orig_freq=48_000, new_freq=16_000)
+
 
 test_dataset = load_dataset("Cnam-LMSSC/vibravox", "speech_clean", split="test", streaming=False)
 
 
 def enhance_audio(sample):
-
-    for sensor, eben in ebens.items():
-        audio = sample[f"audio.{sensor}"]
-        cut_audio = eben.cut_to_valid_length(audio)
-        enhanced_audio = eben(cut_audio)
-        sample[f"audio.{sensor}"] = enhanced_audio
+    for sensor, eben in EBENs.items():
+        audio = torch.Tensor(sample[f"audio.{sensor}"]["array"])[None, None, ...]
+        resampled_audio = resampler(audio)
+        cut_audio = eben.cut_to_valid_length(resampled_audio)
+        enhanced_audio, _ = eben(cut_audio)
+        sample[f"audio.{sensor}"]["array"] = enhanced_audio.detach().numpy().squeeze()
+        sample[f"audio.{sensor}"]["sampling_rate"] = 16_000
 
     return sample
 
 
 enhanced_test_dataset = test_dataset.map(enhance_audio)
+enhanced_test_dataset.push_to_hub(repo_id="Cnam-LMSSC/vibravox_enhanced_by_EBEN_dummy2",
+                                  config_name="speech_clean",
+                                  commit_message="Enhance audio with EBEN dummy")
+
 
 
