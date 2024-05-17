a7666b38c8720017b04b15ee1448b040bf6f49cb
Eric Bavu <eric.bavu@gmail.com>
2024-05-17 18:14:02 +0200

    Testing the train, val, and test LightningDataModules for spkv.

--- tests/lightning_datamodules/spkv_test.py
+++ tests/lightning_datamodules/spkv_test.py
@@ -33,32 +33,101 @@ def main(cfg: DictConfig):
     lightning_datamodule: LightningDataModule = hydra.utils.instantiate(
         cfg.lightning_datamodule
     )
-    # Test the DataModule :
-    lightning_datamodule.setup()
+    # Test the DataModule for test set :
 
-    batch = next(iter(lightning_datamodule.test_dataloader()))
+    print('#############################################################,\n')
+    print("Getting instances of the test set using CombineDataloder : \n")
 
-    print("\n Combined Batch from CombinedDataloder : \n ")
+    lightning_datamodule.setup(stage = 'test')
+
+    test_iterator = iter(lightning_datamodule.test_dataloader())
+
+    batch = next(test_iterator)
+
+    print("\n Combined Batch from CombinedDataloader : \n ")
     print(batch)
-
     print("\n Keys from Dataloader 'sensor_a' :")
     print(batch[0]['sensor_a'].keys())
-
     print(" \n Values from Dataloader 'sensor_a' : \n " )
     print((batch[0]['sensor_a']['audio']))
     print((batch[0]['sensor_a']['speaker_id']))
     print((batch[0]['sensor_a']['sentence_id']))
     print((batch[0]['sensor_a']['gender']))
-
+    print((batch[0]['sensor_a']['sensor']))
     print("\n Keys from Dataloader 'sensor_b' : \n")
     print(batch[0]['sensor_b'].keys())
-
     print("\n Values from Dataloader 'sensor_b' : \n")
-
     print((batch[0]['sensor_b']['audio']))
     print((batch[0]['sensor_b']['speaker_id']))
     print((batch[0]['sensor_b']['sentence_id']))
     print((batch[0]['sensor_b']['gender']))
+    print((batch[0]['sensor_b']['sensor']))
+
+
+    print('#############################################################,\n')
+    print("Getting instances of the train set using standard Dataloader : \n")
+
+    lightning_datamodule.setup(stage='fit')
+    train_iterator = iter(lightning_datamodule.train_dataloader())
+
+    batch = next(train_iterator)
+
+    print("\n Batch from Train Dataloder : \n ")
+    print(batch)
+    print("\n Keys from batch :")
+    print(batch.keys())
+    print(" \n Values from Train Dataloader : \n ")
+    print((batch['audio']))
+    print((batch['speaker_id']))
+    print((batch['sentence_id']))
+    print((batch['gender']))
+    print((batch['sensor']))
+
+    batch = next(train_iterator)
+
+    print("\n Second Batch from Train Dataloader : \n ")
+    print(batch)
+    print("\n Keys from batch :")
+    print(batch.keys())
+    print(" \n Values from Validation Dataloader : \n ")
+    print((batch['audio']))
+    print((batch['speaker_id']))
+    print((batch['sentence_id']))
+    print((batch['gender']))
+    print((batch['sensor']))
+
+
+    print('#############################################################,\n')
+    print("Getting instances of the validation set using standard Dataloader : \n")
+
+    lightning_datamodule.setup(stage='fit')
+    val_iterator = iter(lightning_datamodule.val_dataloader())
+
+    batch = next(val_iterator)
+
+    print("\n Batch from Validation Dataloader : \n ")
+    print(batch)
+    print("\n Keys from batch :")
+    print(batch.keys())
+    print(" \n Values from Validation Dataloader : \n ")
+    print((batch['audio']))
+    print((batch['speaker_id']))
+    print((batch['sentence_id']))
+    print((batch['gender']))
+    print((batch['sensor']))
+
+    batch = next(val_iterator)
+
+    print("\n Second Batch from Validation Dataloder : \n ")
+    print(batch)
+    print("\n Keys from batch :")
+    print(batch.keys())
+    print(" \n Values from Validation Dataloader : \n ")
+    print((batch['audio']))
+    print((batch['speaker_id']))
+    print((batch['sentence_id']))
+    print((batch['gender']))
+    print((batch['sensor']))
 
 
 
