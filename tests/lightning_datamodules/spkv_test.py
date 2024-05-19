ded641fb1c5650db491f8f5696ca9a24870ebc0d
jhauret <j.hauret.33@gmail.com>
2024-05-19 18:51:49 +0200

    write all tests without being able to test locally (no internet to download requirements)

--- tests/lightning_datamodules/spkv_test.py
+++ tests/lightning_datamodules/spkv_test.py
@@ -1,11 +1,10 @@
-import numpy as np
 import torch
 
 from vibravox.lightning_datamodules.spkv import SPKVLightningDataModule
 
 
 class TestSPKVLightningDataModule:
-    def test_dataset_test_return_type(self, spkv_lightning_datamodule_same_sensors_instance):
+    def test_dataset_test_stage_returns_type(self, spkv_lightning_datamodule_same_sensors_instance):
         spkv_lightning_datamodule_same_sensors_instance.setup("test")
         test_dataset_a = spkv_lightning_datamodule_same_sensors_instance.test_dataset_a
         test_dataset_b = spkv_lightning_datamodule_same_sensors_instance.test_dataset_b
@@ -28,7 +27,7 @@ class TestSPKVLightningDataModule:
         assert isinstance(dataset_sample_a["sensor"], str)
         assert isinstance(dataset_sample_b["sensor"], str)
 
-    def test_dataloader_test_returns_format(self, spkv_lightning_datamodule_same_sensors_instance):
+    def test_dataloader_test_stage_returns_format(self, spkv_lightning_datamodule_same_sensors_instance):
         spkv_lightning_datamodule_same_sensors_instance.setup("test")
         test_dataloder = spkv_lightning_datamodule_same_sensors_instance.test_dataloader()
         dataloader_sample = next(iter(test_dataloder))
@@ -40,4 +39,125 @@ class TestSPKVLightningDataModule:
         assert isinstance(dataloader_sample_0["sensor_b"], dict), "Expected dataloader_sample['sensor_b'] to be a dictionary."
         assert dataloader_sample_0["sensor_a"].keys() == {"audio", "speaker_id", "sentence_id", "gender", "sensor"}
         assert dataloader_sample_0["sensor_b"].keys() == {"audio", "speaker_id", "sentence_id", "gender", "sensor"}
+
+        assert isinstance(dataloader_sample_0["sensor_a"]["audio"], torch.Tensor)
+        assert isinstance(dataloader_sample_0["sensor_b"]["audio"], torch.Tensor)
+        assert dataloader_sample_0["sensor_a"]["audio"].ndim == 3
+        assert dataloader_sample_0["sensor_b"]["audio"].ndim == 3
+        assert dataloader_sample_0["sensor_a"]["audio"].shape[1] == 1
+        assert dataloader_sample_0["sensor_b"]["audio"].shape[1] == 1
+
+        assert isinstance(dataloader_sample_0["sensor_a"]["speaker_id"], list)
+        assert isinstance(dataloader_sample_0["sensor_b"]["speaker_id"], list)
+        assert isinstance(dataloader_sample_0["sensor_a"]["speaker_id"][0], str)
+        assert isinstance(dataloader_sample_0["sensor_b"]["speaker_id"][0], str)
+
+        assert isinstance(dataloader_sample_0["sensor_a"]["sentence_id"], list)
+        assert isinstance(dataloader_sample_0["sensor_b"]["sentence_id"], list)
+        assert isinstance(dataloader_sample_0["sensor_a"]["sentence_id"][0], int)
+        assert isinstance(dataloader_sample_0["sensor_b"]["sentence_id"][0], int)
+
+        assert isinstance(dataloader_sample_0["sensor_a"]["gender"], list)
+        assert isinstance(dataloader_sample_0["sensor_b"]["gender"], list)
+        assert isinstance(dataloader_sample_0["sensor_a"]["gender"][0], str)
+        assert isinstance(dataloader_sample_0["sensor_b"]["gender"][0], str)
+        assert dataloader_sample_0["sensor_a"]["gender"][0] in ["male", "female"]
+        assert dataloader_sample_0["sensor_b"]["gender"][0] in ["male", "female"]
+
+        assert isinstance(dataloader_sample_0["sensor_a"]["sensor"], list)
+        assert isinstance(dataloader_sample_0["sensor_b"]["sensor"], list)
+        assert isinstance(dataloader_sample_0["sensor_a"]["sensor"][0], str)
+        assert isinstance(dataloader_sample_0["sensor_b"]["sensor"][0], str)
+
+        assert dataloader_sample_0["sensor_a"]["sensor"][0] in ["body_conducted.temple.contact_microphone",
+                                                        "body_conducted.throat.piezoelectric_sensor",
+                                                        "body_conducted.in_ear.rigid_earpiece_microphone",
+                                                        "body_conducted.in_ear.comply_foam_microphone",
+                                                        "body_conducted.forehead.miniature_accelerometer",
+                                                        "airborne.mouth_headworn.reference_microphone"]
+
+        assert dataloader_sample_0["sensor_b"]["sensor"][0] in ["body_conducted.temple.contact_microphone",
+                                                      "body_conducted.throat.piezoelectric_sensor",
+                                                      "body_conducted.in_ear.rigid_earpiece_microphone",
+                                                      "body_conducted.in_ear.comply_foam_microphone",
+                                                      "body_conducted.forehead.miniature_accelerometer",
+                                                      "airborne.mouth_headworn.reference_microphone"]
+
+    def test_dataset_fit_stage_returns_type(self, spkv_lightning_datamodule_same_sensors_instance):
+        spkv_lightning_datamodule_same_sensors_instance.setup("fit")
+        val_dataset = spkv_lightning_datamodule_same_sensors_instance.val_dataset
+        train_dataset = spkv_lightning_datamodule_same_sensors_instance.train_dataset
+        dataset_sample_val = next(iter(val_dataset))
+        dataset_sample_train = next(iter(train_dataset))
+
+        assert isinstance(dataset_sample_val["audio"]["array"], torch.Tensor)
+        assert isinstance(dataset_sample_train["audio"]["array"], torch.Tensor)
+
+        assert isinstance(dataset_sample_val["speaker_id"], str)
+        assert isinstance(dataset_sample_train["speaker_id"], str)
+
+        assert isinstance(dataset_sample_val["sentence_id"], torch.Tensor)
+        assert isinstance(dataset_sample_train["sentence_id"], torch.Tensor)
+
+        assert isinstance(dataset_sample_val["gender"], str)
+        assert isinstance(dataset_sample_train["gender"], str)
+
+        assert isinstance(dataset_sample_val["sensor"], str)
+        assert isinstance(dataset_sample_train["sensor"], str)
+
+    def test_dataloader_fit_stage_returns_format(self, spkv_lightning_datamodule_same_sensors_instance):
+        spkv_lightning_datamodule_same_sensors_instance.setup("fit")
+        train_dataloder = spkv_lightning_datamodule_same_sensors_instance.train_dataloader()
+        val_dataloder = spkv_lightning_datamodule_same_sensors_instance.val_dataloder()
+        train_dataloader_sample = next(iter(train_dataloder))
+        val_dataloader_sample = next(iter(val_dataloder))
+
+        assert isinstance(train_dataloader_sample, dict), "Expected dataloader_sample to be a dictionary."
+        assert isinstance(val_dataloader_sample, dict), "Expected dataloader_sample to be a dictionary."
+        assert train_dataloader_sample.keys() == {"audio", "speaker_id", "sentence_id", "gender", "sensor"}
+        assert val_dataloader_sample.keys() == {"audio", "speaker_id", "sentence_id", "gender", "sensor"}
+
+        assert isinstance(train_dataloader_sample["audio"], torch.Tensor)
+        assert isinstance(val_dataloader_sample["audio"], torch.Tensor)
+        assert train_dataloader_sample["audio"].ndim == 3
+        assert val_dataloader_sample["audio"].ndim == 3
+        assert train_dataloader_sample["audio"].shape[1] == 1
+        assert val_dataloader_sample["audio"].shape[1] == 1
+
+        assert isinstance(train_dataloader_sample["speaker_id"], list)
+        assert isinstance(val_dataloader_sample["speaker_id"], list)
+        assert isinstance(train_dataloader_sample["speaker_id"][0], str)
+        assert isinstance(val_dataloader_sample["speaker_id"][0], str)
+
+        assert isinstance(train_dataloader_sample["sentence_id"], list)
+        assert isinstance(val_dataloader_sample["sentence_id"], list)
+        assert isinstance(train_dataloader_sample["sentence_id"][0], int)
+        assert isinstance(val_dataloader_sample["sentence_id"][0], int)
+
+        assert isinstance(train_dataloader_sample["gender"], list)
+        assert isinstance(val_dataloader_sample["gender"], list)
+        assert isinstance(train_dataloader_sample["gender"][0], str)
+        assert isinstance(val_dataloader_sample["gender"][0], str)
+        assert train_dataloader_sample["gender"][0] in ["male", "female"]
+        assert val_dataloader_sample["gender"][0] in ["male", "female"]
+
+        assert isinstance(train_dataloader_sample["sensor"], list)
+        assert isinstance(val_dataloader_sample["sensor"], list)
+        assert isinstance(train_dataloader_sample["sensor"][0], str)
+        assert isinstance(val_dataloader_sample["sensor"][0], str)
+
+        assert train_dataloader_sample["sensor"][0] in ["body_conducted.temple.contact_microphone",
+                                                        "body_conducted.throat.piezoelectric_sensor",
+                                                        "body_conducted.in_ear.rigid_earpiece_microphone",
+                                                        "body_conducted.in_ear.comply_foam_microphone",
+                                                        "body_conducted.forehead.miniature_accelerometer",
+                                                        "airborne.mouth_headworn.reference_microphone"]
+
+        assert val_dataloader_sample["sensor"][0] in ["body_conducted.temple.contact_microphone",
+                                                        "body_conducted.throat.piezoelectric_sensor",
+                                                        "body_conducted.in_ear.rigid_earpiece_microphone",
+                                                        "body_conducted.in_ear.comply_foam_microphone",
+                                                        "body_conducted.forehead.miniature_accelerometer",
+                                                        "airborne.mouth_headworn.reference_microphone"]
+
 
