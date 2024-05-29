305fb70f1da2bcafd362f9be3fedd06b112ef4b3
Eric Bavu <eric.bavu@gmail.com>
2024-05-29 18:53:15 +0200

    Add SPKV slurm script

---
+++ scripts/run_spkv_slurm_array_JZ.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+#SBATCH --job-name=array_job
+#SBATCH --output=slurm-%A_%a.out
+#SBATCH --error=slurm-%A_%a.err
+#SBATCH --constraint=v100-16g
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=10
+#SBATCH --time=99:00:00
+#SBATCH --qos=qos_gpu-t4
+#SBATCH --hint=nomultithread
+#SBATCH --account=lbo@v100
+#SBATCH --array=1-35
+
+module purge
+conda deactivate
+
+module load pytorch-gpu/py3/2.2.0
+
+export HF_DATASETS_CACHE=$WORK/huggingface_cache/datasets
+export HF_DATASETS_OFFLINE=1
+
+# Specify the path to the config file
+array_config=./configs/slurm_array/spkv.txt
+
+# Extract values of the job
+dataset_name=$(awk -v ArrayTaskID=$SLURM_ARRAY_TASK_ID '$1==ArrayTaskID {print $2}' $array_config)
+split=$(awk -v ArrayTaskID=$SLURM_ARRAY_TASK_ID '$1==ArrayTaskID {print $3}' $array_config)
+sensor_a=$(awk -v ArrayTaskID=$SLURM_ARRAY_TASK_ID '$1==ArrayTaskID {print $4}' $array_config)
+sensor_b=$(awk -v ArrayTaskID=$SLURM_ARRAY_TASK_ID '$1==ArrayTaskID {print $5}' $array_config)
+pairs=$(awk -v ArrayTaskID=$SLURM_ARRAY_TASK_ID '$1==ArrayTaskID {print $6}' $array_config)
+
+set -x
+srun python -u run.py lightning_datamodule=spkv lightning_module=ecapa2 lightning_datamodule.dataset_name="$dataset_name"  lightning_datamodule.split="$split" lightning_datamodule.sensor_a="$sensor_a" lightning_datamodule.sensor_b="$sensor_b" lightning_datamodule.pairs="$pairs" ++trainer.limit_train_batches=0
