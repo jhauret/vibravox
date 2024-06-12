8c9f968a68a2a27d939bbd5484acc223818cfcca
jhauret <j.hauret.33@gmail.com>
2024-06-12 17:29:14 +0200

    lightning_module.push_to_hub_after_testing

--- scripts/run_all_stp.sh
+++ scripts/run_all_stp.sh
@@ -1,9 +1,9 @@
 pyenv which python;
 echo "Will run all STP trainings sequentially";
 sleep 5;
-python run.py lightning_datamodule=stp lightning_module=wav2vec2_for_stp lightning_module.optimizer.lr=1e-5 lightning_datamodule.sensor=headset_microphone ++trainer.max_epochs=10;
-python run.py lightning_datamodule=stp lightning_module=wav2vec2_for_stp lightning_module.optimizer.lr=1e-5 lightning_datamodule.sensor=forehead_accelerometer ++trainer.max_epochs=10;
-python run.py lightning_datamodule=stp lightning_module=wav2vec2_for_stp lightning_module.optimizer.lr=1e-5 lightning_datamodule.sensor=soft_in_ear_microphone ++trainer.max_epochs=10;
-python run.py lightning_datamodule=stp lightning_module=wav2vec2_for_stp lightning_module.optimizer.lr=1e-5 lightning_datamodule.sensor=rigid_in_ear_microphone ++trainer.max_epochs=10;
-python run.py lightning_datamodule=stp lightning_module=wav2vec2_for_stp lightning_module.optimizer.lr=1e-5 lightning_datamodule.sensor=temple_vibration_pickup ++trainer.max_epochs=10;
-python run.py lightning_datamodule=stp lightning_module=wav2vec2_for_stp lightning_module.optimizer.lr=1e-5 lightning_datamodule.sensor=throat_microphone ++trainer.max_epochs=10;
+python run.py lightning_datamodule=stp lightning_module=wav2vec2_for_stp lightning_module.optimizer.lr=1e-5 lightning_datamodule.sensor=headset_microphone ++trainer.max_epochs=10 ++lightning_module.push_to_hub_after_testing=True;
+python run.py lightning_datamodule=stp lightning_module=wav2vec2_for_stp lightning_module.optimizer.lr=1e-5 lightning_datamodule.sensor=forehead_accelerometer ++trainer.max_epochs=10 ++lightning_module.push_to_hub_after_testing=True;
+python run.py lightning_datamodule=stp lightning_module=wav2vec2_for_stp lightning_module.optimizer.lr=1e-5 lightning_datamodule.sensor=soft_in_ear_microphone ++trainer.max_epochs=10 ++lightning_module.push_to_hub_after_testing=True;
+python run.py lightning_datamodule=stp lightning_module=wav2vec2_for_stp lightning_module.optimizer.lr=1e-5 lightning_datamodule.sensor=rigid_in_ear_microphone ++trainer.max_epochs=10 ++lightning_module.push_to_hub_after_testing=True;
+python run.py lightning_datamodule=stp lightning_module=wav2vec2_for_stp lightning_module.optimizer.lr=1e-5 lightning_datamodule.sensor=temple_vibration_pickup ++trainer.max_epochs=10 ++lightning_module.push_to_hub_after_testing=True;
+python run.py lightning_datamodule=stp lightning_module=wav2vec2_for_stp lightning_module.optimizer.lr=1e-5 lightning_datamodule.sensor=throat_microphone ++trainer.max_epochs=10 ++lightning_module.push_to_hub_after_testing=True;
