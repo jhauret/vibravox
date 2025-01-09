02685d339ce676dde10b2055471789b25ce1a158
Malo OLIVIER <MaloOLIVIER333@gmail.com>
2025-01-09 17:09:37 +0100

    started pytest file for NoisyBWELightningDataModule

---
+++ tests/lightning_datamodules/noisybwe_test.py
@@ -0,0 +1,8 @@
+import torch
+import pytest
+from vibravox.lightning_datamodules.noisybwe import NoisyBWELightningDataModule
+
+
+class TestNoisyBWELightningDataModule:
+    def mock():
+        pass
