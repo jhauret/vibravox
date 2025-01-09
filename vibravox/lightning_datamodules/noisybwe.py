9f3af35b65b5fb71a509926d980072244cb09b5e
Malo OLIVIER <MaloOLIVIER333@gmail.com>
2025-01-09 17:05:09 +0100

    created new branch : development task is to implement a new lightning_datamodule with data augmentation for bandwidth extension

---
+++ vibravox/lightning_datamodules/noisybwe.py
@@ -0,0 +1,10 @@
+import torch
+import re
+from typing import Dict, List
+
+from lightning import LightningDataModule
+
+class NoisyBWELightningDataModule(LightningDataModule):
+    
+    def init():
+        pass
