a343477ea902fbabb58d6ac28d85cf9d8dbd42ab
jhauret <j.hauret.33@gmail.com>
2025-01-14 16:13:27 +0100

    * functional per * utils in utils * CUDA_IS_AVAILABLE

--- scripts/test_all_phonemizers.py
+++ scripts/test_all_phonemizers.py
@@ -1,7 +1,7 @@
 """
 This script is used to test all phonemizers on all microphones, thus creating a 6x6 matrix of the Phoneme Error Rate (PER).
 """
-from typing import List, Tuple
+
 from collections import Counter
 import torchmetrics
 import transformers
@@ -9,114 +9,39 @@ from datasets import load_dataset, Audio
 import torch
 from transformers import AutoProcessor, AutoModelForCTC
 from tqdm import tqdm
+from vibravox.utils import split_editops, decode_operations
 import Levenshtein
 import pickle
+from torchmetrics.functional.text import char_error_rate
 
 MICROPHONES = ["headset_microphone",
                "forehead_accelerometer",
-               "rigid_in_ear_microphone",
                "soft_in_ear_microphone",
+               "rigid_in_ear_microphone",
                "throat_microphone",
                "temple_vibration_pickup"]
 
 PHONEMIZERS = [f"phonemizer_{microphone}" for microphone in MICROPHONES]
-
-
 SAMPLE_RATE = 16_000
 DATASETS = ["Cnam-LMSSC/vibravox", "Cnam-LMSSC/vibravox_enhanced_by_EBEN"]
+CUDA_IS_AVAILABLE = torch.cuda.is_available()
+
 FEATURE_EXTRACTOR = transformers.Wav2Vec2FeatureExtractor()
 TOKENIZER = transformers.Wav2Vec2CTCTokenizer.from_pretrained("Cnam-LMSSC/vibravox-phonemes-tokenizer")
-PER = torchmetrics.text.CharErrorRate()
 
 per_results = torch.empty((len(DATASETS), len(MICROPHONES), len(PHONEMIZERS)))
 editops_occurrences_results = {}
-
-
-def decode_operations(predicted_chr: str,
-                      label_chr: str,
-                      editops: List[Tuple[str, int, int]]) -> List[Tuple[str, str, str]]:
-    """
-    Decode the operations based on the edit operations.
-
-    Args:
-        predicted_chr (str): The predicted character.
-        label_chr (str): The label character.
-        editops (List[Tuple[str, int, int]]): The list of edit operations.
-
-    Returns:
-        List[Tuple[str, str, str]]: The list of decoded operations.
-    """
-    ops = []
-    for editop in editops:
-        op, pred_idx, label_idx = editop
-
-        if op == "insert":
-            label_token = label_chr[label_idx]
-            ops.append((op, label_token, label_token))
-        elif op == "delete":
-            pred_token = predicted_chr[pred_idx]
-            ops.append((op, pred_token, pred_token))
-        else:
-            label_token = label_chr[label_idx]
-            pred_token = predicted_chr[pred_idx]
-            ops.append((op, pred_token, label_token))
-
-    return ops
-
-
-def get_space_indices(string: str) -> List[int]:
-    """
-    Get the positions of spaces in a string.
-
-    Args:
-        string (str): The input string.
-
-    Returns:
-        List[int]: The list of space indices.
-    """
-    return [i for i, x in enumerate(string) if x == ' ']
-
-
-def split_editops(pred: str,
-                  target: str,
-                  editops: List[Tuple[str, int, int]])\
-        -> Tuple[List[Tuple[str, int, int]], List[Tuple[str, int, int]], List[Tuple[str, int, int]]]:
-    """
-    Split the edit operations into three categories: before space, in word, and all.
-
-    Args:
-        pred (str): The predicted string.
-        target (str): The target string.
-        editops (List[Tuple[str, int, int]]): The list of edit operations.
-
-    Returns:
-        Tuple[List[Tuple[str, int, int]], List[Tuple[str, int, int]], List[Tuple[str, int, int]]]: The split edit operations.
-    """
-    pred_space_idx = get_space_indices(pred)
-    target_space_idx = get_space_indices(target)
-
-    raw_editops_before_space = []
-    raw_editops_in_word = []
-    for editop in editops:
-        op, pred_idx, label_idx = editop
-
-        if ((op == 'replace' and ((pred_idx+1) in pred_space_idx or (label_idx + 1) in target_space_idx)) or
-            (op == 'delete' and (pred_idx+1) in pred_space_idx) or
-            (op == 'insert' and (label_idx + 1) in target_space_idx)):
-            raw_editops_before_space.append(editop)
-        else:
-            raw_editops_in_word.append(editop)
-
-    return raw_editops_before_space, raw_editops_in_word, editops
-
 
 for dataset_idx, dataset_name in enumerate(DATASETS):
     for microphone_idx, microphone in enumerate(MICROPHONES):
         test_dataset = load_dataset(dataset_name, "speech_clean", split="test", streaming=False)
         test_dataset = test_dataset.cast_column(f"audio.{microphone}", Audio(sampling_rate=SAMPLE_RATE, mono=False))
+        test_dataset = test_dataset.with_format("torch")
         for phonemizer_idx, phonemizer in enumerate(PHONEMIZERS):
             processor = AutoProcessor.from_pretrained(f"Cnam-LMSSC/{phonemizer}")
             model = AutoModelForCTC.from_pretrained(f"Cnam-LMSSC/{phonemizer}")
+            if CUDA_IS_AVAILABLE:
+                model.cuda()
             preds, targets, editops_in_word, editops_before_space, editops_all = [], [], [], [], []
             for sample in tqdm(test_dataset):
 
@@ -125,6 +50,8 @@ for dataset_idx, dataset_name in enumerate(DATASETS):
                 # Compute predicted transcription
                 audio_16kHz = sample[f"audio.{microphone}"]["array"]
                 inputs = processor(audio_16kHz, sampling_rate=16_000, return_tensors="pt")
+                if CUDA_IS_AVAILABLE:
+                    inputs.input_values = inputs.input_values.cuda()
                 logits = model(inputs.input_values).logits
                 predicted_ids = torch.argmax(logits, dim=-1)
                 predicted_transcription = processor.batch_decode(predicted_ids)[0]
@@ -145,7 +72,7 @@ for dataset_idx, dataset_name in enumerate(DATASETS):
                 editops_all += decoded_operations_all
 
             # Save PER
-            per = PER(preds, targets)
+            per = char_error_rate(preds, targets)
             print(f"Test PER of {phonemizer} on {microphone} for {dataset_name} subset: {per * 100:.2f}%")
             per_results[dataset_idx, microphone_idx, phonemizer_idx] = per
 
