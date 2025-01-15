be3b57b168c770282beee21cc4b882baa59b7e2a
Malo OLIVIER <MaloOLIVIER333@gmail.com>
2025-01-15 10:19:57 +0100

    Add SpeechNoiseDataset for combining speech and noise datasets
    - Implemented SpeechNoiseDataset class inheriting from torch.utils.data.Dataset
    - Facilitates pairing of speech and noise samples for audio augmentation
    - Implemented __len__() which is the length of the speech dataset (because len(noise_dataset) < len(speech_dataset))
    - Implemented __getitem__() to return the ref audio, the pred body conducted audio and the noise body conducted audio

---
+++ vibravox/datasets/speech_noise.py
@@ -0,0 +1,30 @@
+from typing import List, Any, Dict, Union
+from torch.utils.data import Dataset
+
+class SpeechNoiseDataset(Dataset):
+    """_summary_
+
+    Args:
+        Dataset (_type_): _description_
+    """
+    
+    def __init__(self, speech_dataset: Dataset, noise_dataset: Dataset):
+        
+        super().__init__()
+        
+        self.speech_dataset = speech_dataset
+        self.noise_dataset = noise_dataset
+        
+    def __len__(self) -> int:
+        return len(self.speech_dataset)
+    
+    def __getitem__(self, idx: int) -> Dict[str, Any]:
+        
+        speech_sample = self.speech_dataset[idx]
+        noise_sample = self.noise_dataset[idx]
+        
+        return {
+            "audio_airborne": speech_sample["audio_airborne"],
+            "audio_body_conducted": speech_sample["audio_body_conducted"],
+            "audio_body_conducted_speechless_noisy": noise_sample["audio_body_conducted_speechless_noisy"],
+        }
