5820ecf43c486ca73cafc4b142987386877e000d
Malo OLIVIER <MaloOLIVIER333@gmail.com>
2025-01-15 10:25:03 +0100

    Changed implementation idea for NoisyBWELightningDataModule
    - NoisyBWELightningDataModule should use SpeechNoiseDataset(speech_dataset, noise_dataset) and collator function to mix speech and noise
    
    Right now, the difference with the precedent commit relative to this file is :
    
    - Import SpeechNoiseDataset for handling speech and noise data
    - Rename and select audio columns directly within the dataset loading process
    - Cast audio columns to the appropriate Audio feature with specified sampling rate and channels
    - Remove deprecated helper methods ('_rename_column', '_add_noise_columns', '_cast_column')
    - Initialize training and validation datasets using SpeechNoiseDataset
    - Update test dataset concatenation logic
    - Comment out unused augmentation steps to simplify the data pipeline

--- vibravox/lightning_datamodules/noisybwe.py
+++ vibravox/lightning_datamodules/noisybwe.py
@@ -10,6 +10,7 @@ from lightning import LightningDataModule
 from vibravox.utils import mix_speech_and_noise
 from vibravox.utils import set_audio_duration
 from vibravox.torch_modules.dsp.data_augmentation import WaveformDataAugmentation
+from vibravox.datasets.speech_noise import SpeechNoiseDataset
 
 class NoisyBWELightningDataModule(LightningDataModule):
     
@@ -52,7 +53,7 @@ class NoisyBWELightningDataModule(LightningDataModule):
             f"dataset_name {dataset_name} not supported."
         self.dataset_name = dataset_name
         self.subset = subset
-        self.subset_speechless_noisy = "speechless_noisy" # for data augmentation
+        self.subset_speechless_noisy = "speechless_noisy" # for distribution change 
         self.subset_speech_noisy = "speech_noisy" # for test set
         self.sensor = sensor
 
@@ -83,176 +84,216 @@ class NoisyBWELightningDataModule(LightningDataModule):
             That is why it is necessary to define attributes here rather than in __init__.
         """
         # Load datasets
-        dataset_dict_speechclean = load_dataset(
+        speechclean = load_dataset(
             self.dataset_name, self.subset, streaming=self.streaming
         )
-        dataset_dict_speechless_noisy = load_dataset(
+        speechless_noisy = load_dataset(
             self.dataset_name, self.subset_speechless_noisy, streaming=self.streaming
         )
-        dataset_dict_speech_noisy = load_dataset(
+        speech_noisy = load_dataset(
             self.dataset_name, self.subset_speech_noisy, streaming=self.streaming
         )
 
         # Define datasets with their respective suffixes
-        datasets = {
-            "speechclean": {
-                "dataset": dataset_dict_speechclean,
-                "suffix": None
-            },
-            "speechless_noisy": {
-                "dataset": dataset_dict_speechless_noisy,
-                "suffix": "speechless_noisy"
-            },
-            "speech_noisy": {
-                "dataset": dataset_dict_speech_noisy,
-                "suffix": None
-            }
-        }
+        # datasets = {
+        #     "speechclean": {
+        #         "dataset": dataset_dict_speechclean,
+        #         "suffix": None
+        #     },
+        #     "speechless_noisy": {
+        #         "dataset": dataset_dict_speechless_noisy,
+        #         "suffix": "speechless_noisy"
+        #     },
+        #     "speech_noisy": {
+        #         "dataset": dataset_dict_speech_noisy,
+        #         "suffix": None
+        #     }
+        # }
+        
+        # Process each dataset: rename columns, select columns, cast and format
+        
+        speechclean = speechclean.rename_column("audio.headset_microphone", "audio_airborne")
+        speechclean = speechclean.rename_column(f"audio.{self.sensor}", "audio_body_conducted")
+        speechclean = speechclean.select_columns(["audio_airborne", "audio_body_conducted"])
+        speechclean = speechclean.cast_column(
+            "audio_airborne", Audio(sampling_rate=self.sample_rate, mono=False)
+        )
+        speechclean = speechclean.cast_column(
+            "audio_body_conducted", Audio(sampling_rate=self.sample_rate, mono=False)
+        )
+        speechclean = speechclean.with_format("torch")
+        
+        speechless_noisy = speechless_noisy.rename_column(f"audio.{self.sensor}", "audio_body_conducted_speechless_noisy")
+        speechless_noisy = speechless_noisy.select_columns(["audio_body_conducted_speechless_noisy"])
+        speechless_noisy = speechless_noisy.cast_column(
+            "audio_body_conducted_speechless_noisy", Audio(sampling_rate=self.sample_rate, mono=False)
+        )
+        speechless_noisy = speechless_noisy.with_format("torch")
+            
+        speech_noisy = speech_noisy.rename_column(f"audio.{self.sensor}", "audio_body_conducted_speech_noisy")
+        speech_noisy = speech_noisy.select_columns(["audio_body_conducted_speech_noisy"])
+        speech_noisy = speech_noisy.cast_column(
+            "audio_body_conducted_speech_noisy", Audio(sampling_rate=self.sample_rate, mono=False)
+        )
+        speech_noisy = speech_noisy.with_format("torch")
+        
+        
 
         # Process each dataset: rename columns, select columns, cast and format
-        for key, value in datasets.items():
-            dataset = value["dataset"]
-            suffix = value["suffix"]
-            dataset = self._rename_column(dataset, suffix)
-            datasets[key]["dataset"] = dataset
+        # for key, value in datasets.items():
+        #     dataset = value["dataset"]
+        #     suffix = value["suffix"]
+        #     dataset = self._rename_column(dataset, suffix)
+        #     datasets[key]["dataset"] = dataset
             
-        datasets=self._add_noise_columns(datasets)
+        # datasets=self._add_noise_columns(datasets)
         
-        for key, value in datasets.items():
-            dataset = value["dataset"]
-            suffix = value["suffix"]
-            dataset = self._cast_column(dataset, suffix)
-            datasets[key]["dataset"] = dataset
+        # for key, value in datasets.items():
+        #     dataset = value["dataset"]
+        #     suffix = value["suffix"]
+        #     dataset = self._cast_column(dataset, suffix)
+        #     datasets[key]["dataset"] = dataset
 
-        # Augment datasets
-        datasets["speechclean"]["dataset"] = self.augment_dataset(datasets["speechclean"]["dataset"])
+        # # Augment datasets
+        # datasets["speechclean"]["dataset"] = self.augment_dataset(datasets["speechclean"]["dataset"])
 
         # Assign datasets based on the stage
         if stage in ["fit", None]:
-            self.train_dataset = datasets["speechclean"]["dataset"]['train']  # e.g., 20k rows
-            self.val_dataset = datasets["speechclean"]["dataset"]['validation']  # e.g., 2.5k rows
-        if stage in ["test", None]:
-            self.test_dataset = datasets["speech_noisy"]["dataset"]  # e.g., 1.52k rows
-
-    def _rename_column(self, dataset: Dataset, suffix: Union[str, None]) -> Dataset:
-        """
-        Helper function to rename, select, cast, and format audio columns in a dataset.
-
-        Args:
-            dataset (Dataset): The dataset to process.
-            suffix (str, optional): Suffix to append to the audio column names. Defaults to None.
-
-        Returns:
-            Dataset: The processed dataset.
-        """
-        # Define new column names
-        audio_airborne = "audio_airborne" + (f"_{suffix}" if suffix else "")
-        audio_body_conducted = "audio_body_conducted" + (f"_{suffix}" if suffix else "")
-
-        # Rename columns
-        dataset = dataset.rename_column("audio.headset_microphone", audio_airborne)
-        dataset = dataset.rename_column(f"audio.{self.sensor}", audio_body_conducted)
-
-        # Select relevant columns
-        dataset = dataset.select_columns([audio_airborne, audio_body_conducted])
-
-        return dataset
-    
-    def _add_noise_columns(self, datasets: Dict[str, Dict[str, Dataset]]) -> Dict[str, Dict[str, Dataset]]:
-        """_summary_
-
-        Args:
-            datasets (Dict[str, Dict[str, Dataset]]): _description_
-
-        Returns:
-            Dict[str, Dict[str, Dataset]]: _description_
-        """
-        # Define column names
-        audio_airborne_speechless_noisy = "audio_airborne_speechless_noisy"
-        audio_body_conducted_speechless_noisy = "audio_body_conducted_speechless_noisy"
-        
-        # Add columns
-        for i in ["train", "validation"]:
-            datasets["speechclean"]["dataset"][i] = datasets["speechclean"]["dataset"][i].add_column(audio_airborne_speechless_noisy, datasets["speechless_noisy"]["dataset"][i][audio_airborne_speechless_noisy])
-            datasets["speechclean"]["dataset"][i] = datasets["speechclean"]["dataset"][i].add_column(audio_body_conducted_speechless_noisy, datasets["speechless_noisy"]["dataset"][i][audio_body_conducted_speechless_noisy])
-        
-        return datasets
-    
-    def _cast_column(self, dataset: Dataset, suffix: Union[str, None]) -> Dataset:
-        """_summary_
-
-        Args:
-            dataset (Dataset): _description_
-            suffix (Union[str, None]): _description_
-
-        Returns:
-            Dataset: _description_
-        """
-        # Define new column names
-        audio_airborne = "audio_airborne" + (f"_{suffix}" if suffix else "")
-        audio_body_conducted = "audio_body_conducted" + (f"_{suffix}" if suffix else "")
-
-        # Cast audio columns
-        dataset = dataset.cast_column(
-            audio_airborne, Audio(sampling_rate=self.sample_rate, mono=False)
-        )
-        dataset = dataset.cast_column(
-            audio_body_conducted, Audio(sampling_rate=self.sample_rate, mono=False)
-        )
-
-        # Set format to torch
-        dataset = dataset.with_format("torch")
-
-        return dataset
-
-    def augment_dataset(self, dataset: Union[Dataset, DatasetDict]) -> Union[Dataset, DatasetDict]:
-        """
-        Augment the speech dataset with noise by mixing audio samples.
-
-        Args:
-            dataset (Union[Dataset, DatasetDict]): The dataset to augment the data.
-
-        Returns:
-            Union[Dataset, DatasetDict]: The augmented dataset with mixed audio.
-        """
-        
-        _dataset = dataset
-        
-        def map_fn(audio_key: str, noise_key: str):
-            def _map(batch):
-                list1 = batch[audio_key]
-                list2 = batch[noise_key]
-                mixed_audio = []
-                noise_cycle = cycle(list2)
-                # Filter None values out of the lists
-                # list1 = list(filter(None, list1))
-                # noise_cycle = cycle(filter(None, list2))
-                for audio1 in list1:
-                    audio2 = next(noise_cycle)
-                    sample1 = audio1["array"]
-                    sample2 = audio2["array"]
-                    mixed, _ = mix_speech_and_noise(sample1, sample2)
             
-                    audio_mixed = {'path': audio1['path'], 'array': mixed, 'sampling_rate': audio1['sampling_rate']}
+            speech_train = speechclean["train"]
+            speech_validation = speechclean["validation"]
+            noise_train = speechless_noisy["train"]
+            noise_validation = speechless_noisy["validation"]
+            
+            self.train_dataset = SpeechNoiseDataset(speech_train, noise_train)
+            self.val_dataset = SpeechNoiseDataset(speech_validation, noise_validation)
+            
+        if stage in ["test", None]:
+            
+            # Concatenate speech_noisy splits
+            speech_noisy_real = concatenate_datasets([speech_noisy["train"], speech_noisy["validation"], speech_noisy["test"]])
+            
+            self.test_dataset = speech_noisy_real
+
+    # def _rename_column(self, dataset: Dataset, suffix: Union[str, None]) -> Dataset:
+    #     """
+    #     Helper function to rename, select, cast, and format audio columns in a dataset.
+
+    #     Args:
+    #         dataset (Dataset): The dataset to process.
+    #         suffix (str, optional): Suffix to append to the audio column names. Defaults to None.
+
+    #     Returns:
+    #         Dataset: The processed dataset.
+    #     """
+    #     # Define new column names
+    #     audio_airborne = "audio_airborne" + (f"_{suffix}" if suffix else "")
+    #     audio_body_conducted = "audio_body_conducted" + (f"_{suffix}" if suffix else "")
+
+    #     # Rename columns
+    #     dataset = dataset.rename_column("audio.headset_microphone", audio_airborne)
+    #     dataset = dataset.rename_column(f"audio.{self.sensor}", audio_body_conducted)
+
+    #     # Select relevant columns
+    #     dataset = dataset.select_columns([audio_airborne, audio_body_conducted])
+
+    #     return dataset
+    
+    # def _add_noise_columns(self, datasets: Dict[str, Dict[str, Dataset]]) -> Dict[str, Dict[str, Dataset]]:
+    #     """_summary_
+
+    #     Args:
+    #         datasets (Dict[str, Dict[str, Dataset]]): _description_
+
+    #     Returns:
+    #         Dict[str, Dict[str, Dataset]]: _description_
+    #     """
+    #     # Define column names
+    #     audio_airborne_speechless_noisy = "audio_airborne_speechless_noisy"
+    #     audio_body_conducted_speechless_noisy = "audio_body_conducted_speechless_noisy"
+        
+    #     # Add columns
+    #     for i in ["train", "validation"]:
+    #         datasets["speechclean"]["dataset"][i] = datasets["speechclean"]["dataset"][i].add_column(audio_airborne_speechless_noisy, datasets["speechless_noisy"]["dataset"][i][audio_airborne_speechless_noisy])
+    #         datasets["speechclean"]["dataset"][i] = datasets["speechclean"]["dataset"][i].add_column(audio_body_conducted_speechless_noisy, datasets["speechless_noisy"]["dataset"][i][audio_body_conducted_speechless_noisy])
+        
+    #     return datasets
+    
+    # def _cast_column(self, dataset: Dataset, suffix: Union[str, None]) -> Dataset:
+    #     """_summary_
+
+    #     Args:
+    #         dataset (Dataset): _description_
+    #         suffix (Union[str, None]): _description_
+
+    #     Returns:
+    #         Dataset: _description_
+    #     """
+    #     # Define new column names
+    #     audio_airborne = "audio_airborne" + (f"_{suffix}" if suffix else "")
+    #     audio_body_conducted = "audio_body_conducted" + (f"_{suffix}" if suffix else "")
+
+    #     # Cast audio columns
+    #     dataset = dataset.cast_column(
+    #         audio_airborne, Audio(sampling_rate=self.sample_rate, mono=False)
+    #     )
+    #     dataset = dataset.cast_column(
+    #         audio_body_conducted, Audio(sampling_rate=self.sample_rate, mono=False)
+    #     )
+
+    #     # Set format to torch
+    #     dataset = dataset.with_format("torch")
+
+    #     return dataset
+
+    # def augment_dataset(self, dataset: Union[Dataset, DatasetDict]) -> Union[Dataset, DatasetDict]:
+    #     """
+    #     Augment the speech dataset with noise by mixing audio samples.
+
+    #     Args:
+    #         dataset (Union[Dataset, DatasetDict]): The dataset to augment the data.
+
+    #     Returns:
+    #         Union[Dataset, DatasetDict]: The augmented dataset with mixed audio.
+    #     """
+        
+    #     _dataset = dataset
+        
+    #     def map_fn(audio_key: str, noise_key: str):
+    #         def _map(batch):
+    #             list1 = batch[audio_key]
+    #             list2 = batch[noise_key]
+    #             mixed_audio = []
+    #             noise_cycle = cycle(list2)
+    #             # Filter None values out of the lists
+    #             # list1 = list(filter(None, list1))
+    #             # noise_cycle = cycle(filter(None, list2))
+    #             for audio1 in list1:
+    #                 audio2 = next(noise_cycle)
+    #                 sample1 = audio1["array"]
+    #                 sample2 = audio2["array"]
+    #                 mixed, _ = mix_speech_and_noise(sample1, sample2)
+            
+    #                 audio_mixed = {'path': audio1['path'], 'array': mixed, 'sampling_rate': audio1['sampling_rate']}
                     
-                    mixed_audio.append(audio_mixed)
-                return {audio_key: mixed_audio}
-            return _map
+    #                 mixed_audio.append(audio_mixed)
+    #             return {audio_key: mixed_audio}
+    #         return _map
 
-        # Augment 'audio_airborne'
-        for i in ["train", "validation"]:
-            _dataset[i] = _dataset[i].map(
-                map_fn("audio_airborne", "audio_airborne_speechless_noisy"),
-                remove_columns=["audio_airborne_speechless_noisy"],
-                batched=True,
-            )
-            _dataset[i] = _dataset[i].map(
-                map_fn("audio_body_conducted", "audio_body_conducted_speechless_noisy"),
-                remove_columns=["audio_body_conducted_speechless_noisy"],
-                batched=True,
-            )
+    #     # Augment 'audio_airborne'
+    #     for i in ["train", "validation"]:
+    #         _dataset[i] = _dataset[i].map(
+    #             map_fn("audio_airborne", "audio_airborne_speechless_noisy"),
+    #             remove_columns=["audio_airborne_speechless_noisy"],
+    #             batched=True,
+    #         )
+    #         _dataset[i] = _dataset[i].map(
+    #             map_fn("audio_body_conducted", "audio_body_conducted_speechless_noisy"),
+    #             remove_columns=["audio_body_conducted_speechless_noisy"],
+    #             batched=True,
+    #         )
 
-        return _dataset
+    #     return _dataset
 
     def train_dataloader(self):
         """
