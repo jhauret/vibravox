274685fec66b4e87cc98d91d5c4824342b556778
Julien Hauret <53187038+jhauret@users.noreply.github.com>
2025-01-28 16:42:52 +0100

    HF_HUB too

--- scripts/single_run_JZ.sh
+++ scripts/single_run_JZ.sh
@@ -19,7 +19,9 @@ conda deactivate                                      # desactiver les environne
 module load pytorch-gpu/py3/2.2.0                     # charger les modules
 
 export HF_DATASETS_CACHE=$WORK/huggingface_cache/datasets
+export HUGGINGFACE_HUB_CACHE=$WORK/huggingface_cache/hub
 export HF_DATASETS_OFFLINE=1
+export HF_HUB_OFFLINE=1
 
 set -x                                                # activer l'echo des commandes
 srun python -u run.py lightning_datamodule=bwe lightning_module=eben ++trainer.check_val_every_n_epoch=15 +callbacks=bwe_checkpoint
