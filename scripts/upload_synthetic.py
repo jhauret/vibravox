b213758f4288afcb1a4454d691b32049fd9a05dc
jhauret <j.hauret.33@gmail.com>
2025-02-14 18:40:26 +0100

    map

--- scripts/upload_synthetic.py
+++ scripts/upload_synthetic.py
@@ -1,23 +1,19 @@
 import torch
-from copy import deepcopy
 from datasets import load_dataset
-from tqdm import tqdm
 
 speech_clean = load_dataset("Cnam-LMSSC/vibravox", "speech_clean")
 speechless_noisy = load_dataset("Cnam-LMSSC/vibravox", "speechless_noisy")
-speech_mixed = deepcopy(speech_clean["test"])
+
 speech_clean = speech_clean.with_format("torch")
 speechless_noisy = speechless_noisy.with_format("torch")
-speech_mixed = speech_mixed.with_format("torch")
 
-for idx, speech_utt in tqdm(enumerate(speech_clean["test"])):
+def mix_speech(example):
     rand_idx = torch.randint(0, len(speechless_noisy["test"]), (1,)).item()
     noise_tt = speechless_noisy["test"][rand_idx]
 
-    speech_samples = speech_utt["audio.headset_microphone"]["array"].size(0)
+    speech_samples = example["audio.headset_microphone"]["array"].size(0)
     noise_samples = noise_tt["audio.headset_microphone"]["array"].size(0)
 
-    # Randomize noise segment
     start_time = torch.randint(0, noise_samples - speech_samples, (1,)).item()
 
     for mic in ["audio.headset_microphone",
@@ -26,8 +22,14 @@ for idx, speech_utt in tqdm(enumerate(speech_clean["test"])):
                 "audio.rigid_in_ear_microphone",
                 "audio.forehead_accelerometer",
                 "audio.temple_vibration_pickup"]:
-
         noise_sliced = noise_tt[mic]["array"][start_time: start_time + speech_samples]
-        speech_mixed[idx][mic]["array"]  = speech_utt[mic]["array"] + noise_sliced
+        example[mic]["array"] = example[mic]["array"] + noise_sliced
 
+    return example
+
+# Apply the transformation
+speech_mixed = speech_clean["test"].map(mix_speech)
+
+# Push to hub
 speech_mixed.push_to_hub("Cnam-LMSSC/vibravox_mixed_for_spkv", "speech_noisy_mixed")
+
